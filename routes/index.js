8cf4f0f580b978d43f721bf8f3c9d45226898efe
ivand <ivandinizaj@gmail.com>
2020-09-17 17:01:00 -0300

    feat: add axios e realizar consulta viacep

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const axios = require('axios').default;
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/:cep', function(req, res, next) {
+
+  const cep = req.params.cep;
+  let re = /[0-9]{8}/g;
+  
+  if(!re.test(cep) || cep.length !== 8)
+    res.status(400).json({'error': 'cep inválido' });
+
+  axios.get(`https://viacep.com.br/ws/${cep}/json/`)
+  .then(function (response) {
+    if(response.data.erro)
+      res.status(400).json({'message': 'cep não encontrado'});
+    else
+      res.status(200).json(response.data);
+  })
+  .catch(function (error) {
+    res.status(400).json({'message': 'cep não encontrado'});
+  });
 });
 
 module.exports = router;
