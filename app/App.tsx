e56665160c6a6b0bd9e3f6df943f61d305881736
ivand <ivandinizaj@gmail.com>
2020-11-10 00:32:25 -0300

    feat: add expo update

--- app/App.tsx
+++ app/App.tsx
@@ -1,8 +1,19 @@
-import React from "react";
-import { View } from "react-native";
+import * as Updates from "expo-updates";
+import React, { useEffect } from "react";
 
 import Routes from "./src/routes";
 
 export default function App() {
+  useEffect(() => {
+    async function updateApp() {
+      const { isAvailable } = await Updates.checkForUpdateAsync();
+      if (isAvailable) {
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+      }
+    }
+    updateApp();
+  }, []);
+
   return <Routes />;
 }
