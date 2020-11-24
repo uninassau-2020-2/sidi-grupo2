530804cb420606bf90e6a89c69c2fdc82497150a
ivand <ivandinizaj@gmail.com>
2020-11-24 08:38:28 -0300

    fix: remover espaçamento

--- app/src/screen/main/cashier/CashierScreen.tsx
+++ app/src/screen/main/cashier/CashierScreen.tsx
@@ -8,18 +8,9 @@ import List from "./ListComponent";
 const CashierScreen: React.FC = () => {
   return (
     <AppProvider>
-      <SafeAreaView style={styles.container}>
-        <List />
-      </SafeAreaView>
+      <List />
     </AppProvider>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "space-between",
-  },
-});
 
 export default CashierScreen;
